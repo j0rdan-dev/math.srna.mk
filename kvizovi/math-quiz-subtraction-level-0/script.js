3719c7608f3d277270e3ba47cb8ee142d762f719
Jordan Atanasovski <jordfocus@gmail.com>
2026-06-24 14:42:47 +0200

    limit max for subtraction beginner quiz

--- kvizovi/math-quiz-subtraction-level-0/script.js
+++ kvizovi/math-quiz-subtraction-level-0/script.js
@@ -16,6 +16,7 @@ function nextQuestion() {
             if (n1 > n2
                 && n1 > 0
                 && n2 > 0 
+                && n1 <= 10
                 && n2 <= 10) {
                 break;
             }
